30de1b9bd9468a7ac46287b8186b25069ae4906b
sunnyjooey@gmail.com <sunnyjooey@gmail.com>
2023-06-02 19:08:39 +0000

    embed process 2w SUDAN

--- GDELT-embed-process.py
+++ GDELT-embed-process.py
@@ -13,7 +13,7 @@ co = CO.lower()
 DATABASE_NAME = 'news_media'
 EVTSLV_TABLE_NAME = f'horn_africa_gdelt_events_{co}_slv'
 EMB_TABLE_NAME = 'horn_africa_gdelt_gsgembed_brz'
-OUTPUT_TABLE_NAME = f'horn_africa_gdelt_gsgembed_{co}_1w_slv'
+OUTPUT_TABLE_NAME = f'horn_africa_gdelt_gsgembed_{co}_2w_slv'
 
 # COMMAND ----------
 
@@ -41,19 +41,14 @@ all_df = all_df.select(*cols)
 
 # COMMAND ----------
 
-# get average embeddings by time intervals (1-week) and admin 1
-m = all_df.groupBy(F.window(F.col("DATEADDED"), "1 week"), 'ADMIN1').mean()
+# get average embeddings by time intervals (2-week) and admin 1
+m = all_df.groupBy(F.window(F.col("DATEADDED"), "2 week"), 'ADMIN1').mean()
 cols = ['DATE', 'ADMIN1'] + list(np.arange(512).astype(str))
 m = m.toDF(*cols)
-display(m)
-
-# COMMAND ----------
 
 # cycle through admin 1s
 admins = m.select('ADMIN1').distinct().rdd.flatMap(list).collect()
 admins = [a for a in admins if a != CO]
-
-# COMMAND ----------
 
 # average admin 1 and whole country embedding
 # this takes whole country embedding if admin 1 is missing
@@ -66,15 +61,58 @@ for admin in admins:
     adm = adm.withColumn('ADMIN1', F.lit(admin))
     collect_dfs.append(adm) 
 
+# collapse into one df and clean
+df1 = functools.reduce(DataFrame.union, collect_dfs)
+df1 = df1.withColumn('STARTDATE', F.to_date(df1['DATE']['start']))
+df1 = df1.withColumn('ENDDATE', F.to_date(df1['DATE']['end']))
+cols = ['STARTDATE', 'ENDDATE', 'ADMIN1'] + list(np.arange(512).astype(str))
+df1 = df1.select(*cols)
+df1.count()
+
 # COMMAND ----------
 
-# collapse into one df and clean
-df = functools.reduce(DataFrame.union, collect_dfs)
-df = df.withColumn('DATE', F.to_date(adm['DATE']['end']))
+# off-set data by 1 week
+all_df = all_df.filter(all_df['DATEADDED'] >= dt.date(2020, 1, 8))
+
+# get average embeddings by time intervals (2-week) and admin 1
+m = all_df.groupBy(F.window(F.col("DATEADDED"), "2 week"), 'ADMIN1').mean()
 cols = ['DATE', 'ADMIN1'] + list(np.arange(512).astype(str))
-df = df.select(*cols)
+m = m.toDF(*cols)
+
+# cycle through admin 1s
+admins = m.select('ADMIN1').distinct().rdd.flatMap(list).collect()
+admins = [a for a in admins if a != CO]
+
+# average admin 1 and whole country embedding
+# this takes whole country embedding if admin 1 is missing
+collect_dfs = []
+for admin in admins:
+    adm = m.filter((m.ADMIN1==admin) | (m.ADMIN1==CO))
+    adm = adm.groupby(F.col('DATE')).mean()
+    cols = ['DATE'] + list(np.arange(512).astype(str))
+    adm = adm.toDF(*cols)
+    adm = adm.withColumn('ADMIN1', F.lit(admin))
+    collect_dfs.append(adm) 
+
+# collapse into one df and clean
+df2 = functools.reduce(DataFrame.union, collect_dfs)
+df2 = df2.withColumn('STARTDATE', F.to_date(df2['DATE']['start']))
+df2 = df2.withColumn('ENDDATE', F.to_date(df2['DATE']['end']))
+cols = ['STARTDATE', 'ENDDATE', 'ADMIN1'] + list(np.arange(512).astype(str))
+df2 = df2.select(*cols)
+df2.count()
+
+# COMMAND ----------
+
+# concat together
+df = df1.union(df2)
+df.count()
 
 # COMMAND ----------
 
 # save
 df.write.mode('append').format('delta').saveAsTable("{}.{}".format(DATABASE_NAME, OUTPUT_TABLE_NAME))
+
+# COMMAND ----------
+
+
