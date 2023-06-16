090b04eff92e3713212252acb4e64a157c4eefc0
sunnyjooey@gmail.com <sunnyjooey@gmail.com>
2023-06-16 13:28:05 +0000

    change embed processing for 100 pc admin1

--- GDELT-embed-process.py
+++ GDELT-embed-process.py
@@ -8,21 +8,21 @@ from pyspark.sql import DataFrame
 
 # COMMAND ----------
 
-from pyspark.sql import SparkSession
-from pyspark.dbutils import DBUtils
+# from pyspark.sql import SparkSession
+# from pyspark.dbutils import DBUtils
 
-spark = SparkSession.builder.getOrCreate()
-dbutils = DBUtils(spark)
-CO = dbutils.widgets.get("CO")
-print(CO)
+# spark = SparkSession.builder.getOrCreate()
+# dbutils = DBUtils(spark)
+# CO = dbutils.widgets.get("CO")
+# print(CO)
 
 # COMMAND ----------
 
-co = CO.lower()
 DATABASE_NAME = 'news_media'
 EVTSLV_TABLE_NAME = f'horn_africa_gdelt_events_a1_slv'
 EMB_TABLE_NAME = 'horn_africa_gdelt_gsgembed_brz'
-OUTPUT_TABLE_NAME = ''
+# CHANGE ME!!
+OUTPUT_TABLE_NAME = 'horn_africa_gdelt_gsgembed_2w_a1_100_slv'
 
 # COMMAND ----------
 
@@ -38,50 +38,47 @@ emb = emb.drop('DATEADDED')
 
 # merge events and embeddings
 all_df = evtslv.join(emb, evtslv.SOURCEURL==emb.url, how='left')
-cols = ['DATEADDED', 'ADMIN1'] + list(np.arange(512).astype(str))
+cols = ['DATEADDED', 'ADMIN1', 'COUNTRY'] + list(np.arange(512).astype(str))
 all_df = all_df.select(*cols)
 
 # COMMAND ----------
 
-# n = all_df.groupBy(F.window(F.col("DATEADDED"), "1 week"), 'ADMIN1').agg(F.count(F.lit(1)).alias("nom"))
-# d = all_df.groupBy(F.window(F.col("DATEADDED"), "1 week")).agg(F.count(F.lit(1)).alias("denom"))
-# d = n.join(d, 'window').withColumn('admin_frac', F.col('nom')/F.col('denom'))
-# display(d)
+countries = ['SU', 'OD', 'ET', 'ER', 'DJ', 'SO', 'UG', 'KE']
 
-# COMMAND ----------
+for CO in countries:
+    df = all_df.filter(all_df.COUNTRY == CO)
 
-# get average embeddings by time intervals (2-week) and admin 1
-m = all_df.groupBy(F.window(F.col("DATEADDED"), "2 week", "1 week", "-3 day"), 'ADMIN1').mean()
-cols = ['DATE', 'ADMIN1'] + list(np.arange(512).astype(str))
-m = m.toDF(*cols)
+    # get average embeddings by time intervals (2-week) and admin 1
+    m = df.groupBy(F.window(F.col("DATEADDED"), "2 week", "1 week", "-3 day"), 'ADMIN1').mean()
+    cols = ['DATE', 'ADMIN1'] + list(np.arange(512).astype(str))
+    m = m.toDF(*cols)
 
-# cycle through admin 1s
-admins = m.select('ADMIN1').distinct().rdd.flatMap(list).collect()
-admins = [a for a in admins if a != CO]
+    # cycle through admin 1s
+    admins = m.select('ADMIN1').distinct().rdd.flatMap(list).collect()
+    admins = [a for a in admins if a != CO]
 
-# average admin 1 and whole country embedding
-# this takes whole country embedding if admin 1 is missing
-collect_dfs = []
-for admin in admins:
-    adm = m.filter((m.ADMIN1==admin) | (m.ADMIN1==CO))
-    adm = adm.groupby(F.col('DATE')).mean()
-    cols = ['DATE'] + list(np.arange(512).astype(str))
-    adm = adm.toDF(*cols)
-    adm = adm.withColumn('ADMIN1', F.lit(admin))
-    collect_dfs.append(adm) 
+    # average admin 1 or take CO embedding if admin 1 is missing
+    collect_dfs = []
+    for admin in admins:
+        adm = m.filter(m.ADMIN1==admin)
+        adm = adm.groupby(F.col('DATE')).mean()
+        if adm.count() == 0:
+            adm = m.filter(m.ADMIN1==CO)
+            adm = adm.groupby(F.col('DATE')).mean()
+        cols = ['DATE'] + list(np.arange(512).astype(str))
+        adm = adm.toDF(*cols)
+        adm = adm.withColumn('ADMIN1', F.lit(admin))
+        collect_dfs.append(adm) 
 
-# collapse into one df and clean
-df = functools.reduce(DataFrame.union, collect_dfs)
-df = df.withColumn('STARTDATE', F.to_date(df['DATE']['start']))
-df = df.withColumn('ENDDATE', F.to_date(df['DATE']['end']))
-cols = ['STARTDATE', 'ENDDATE', 'ADMIN1'] + list(np.arange(512).astype(str))
-df = df.select(*cols)
-print(df.count())
-
-# COMMAND ----------
-
-# save
-df.write.mode('append').format('delta').saveAsTable("{}.{}".format(DATABASE_NAME, OUTPUT_TABLE_NAME))
+    # collapse into one df and clean
+    df = functools.reduce(DataFrame.union, collect_dfs)
+    df = df.withColumn('STARTDATE', F.to_date(df['DATE']['start']))
+    df = df.withColumn('ENDDATE', F.to_date(df['DATE']['end']))
+    df = df.withColumn('COUNTRY', F.lit(CO))
+    cols = ['STARTDATE', 'ENDDATE', 'ADMIN1', 'COUNTRY'] + list(np.arange(512).astype(str))
+    df = df.select(*cols)
+    print(df.count())
+    df.write.mode('append').format('delta').saveAsTable("{}.{}".format(DATABASE_NAME, OUTPUT_TABLE_NAME))
 
 # COMMAND ----------
 
