4fd46090dcb426b468e3c56f8e469c8fd65c0fc3
sunnyjooey@gmail.com <sunnyjooey@gmail.com>
2023-08-29 20:48:27 +0000

    conflict trend static category gen

---
+++ ACLED-feature-static-cat.py
@@ -0,0 +1,104 @@
+# Databricks notebook source
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# COMMAND ----------
+
+DATABASE_NAME = 'news_media'
+INPUT_DATA_TABLE = 'horn_africa_acled_outcome_fatal_escbin_1w_pct_slv'
+OUTPUT_DATA_TABLE = 'horn_africa_acled_conftrend_static_ct1_slv'
+
+# COMMAND ----------
+
+# read in dependent variable
+escalation_data = spark.sql(f"SELECT * FROM {DATABASE_NAME}.{INPUT_DATA_TABLE}")
+escalation_data = escalation_data.toPandas()
+
+# COMMAND ----------
+
+# make data wide
+e = escalation_data[['STARTDATE' ,'ADMIN1',	'COUNTRY', 'FATALSUM']]
+w = e.pivot(index=['COUNTRY','ADMIN1'], columns='STARTDATE', values='FATALSUM')
+
+# COMMAND ----------
+
+# create intermediary vars
+def proportion_over_x(df, x):
+    df[f'prop_{x}'] = df.apply(lambda row: len(row[row >= x]) / len(row), axis=1)
+    return df
+
+def max_over_x(df, x):
+    df[f'max_{x}'] = df.apply(lambda row: 1 if max(row) >= x else 0, axis=1)
+    return df
+
+def num_over_x(df, x):
+    df[f'n_{x}'] = df[df >= x].count(axis=1)
+    return df
+
+w = proportion_over_x(w, 5)
+w = max_over_x(w, 100)
+w = max_over_x(w, 50)
+w = num_over_x(w, 100)
+
+# COMMAND ----------
+
+# create trend categories
+def trend_cat(row):
+    # high conflict
+    if ((row['prop_5'] >= .2) and (row['max_100'] == 1)) or (row['n_100'] >=3):
+        return 1
+    # low conflict
+    elif (row['prop_5'] < .1) and (row['max_50'] == 0): 
+        return 3
+    # medium conflict
+    else:
+        return 2
+
+w['trend_cat1'] = w.apply(lambda row: trend_cat(row), axis=1)
+w.groupby('trend_cat1').size()
+
+# COMMAND ----------
+
+dummies = pd.get_dummies(w['trend_cat1']).rename(columns=lambda x: 'conflict_trend_' + str(x))
+w = pd.concat([w, dummies], axis=1)
+w = w.drop(['trend_cat1'], axis=1)
+
+# COMMAND ----------
+
+# %matplotlib inline
+
+# def graph_it(long_df, wide_df, filter_col):
+#     cdf = pd.merge(long_df, wide_df.loc[:, filter_col], on='ADMIN1', how='left')
+#     cdf = cdf[cdf[filter_col] == 1]
+    
+#     # Plotting the fatalities over time by country
+#     for adm1 in cdf.ADMIN1.unique():
+#         plt.figure(figsize=(30, 10))
+#         adm1_data = cdf[cdf['ADMIN1'] == adm1]
+#         adm1_data = adm1_data.sort_values('STARTDATE')
+#         plt.plot(adm1_data['STARTDATE'], adm1_data['FATALSUM'])
+#         plt.ylim(0, 500)
+        
+#         plt.xlabel('Time')
+#         plt.xticks(rotation=90)
+#         plt.ylabel('Fatalities')
+#         plt.title(adm1)
+#         plt.show()
+
+# graph_it(escalation_data, w, 'conflict_trend_1')
+
+# COMMAND ----------
+
+w = w[['prop_5','max_100','max_50','n_100', 'conflict_trend_1','conflict_trend_2']]
+w = w.reset_index()
+
+# COMMAND ----------
+
+# convert to spark dataframe
+spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
+w = spark.createDataFrame(w)
+
+# COMMAND ----------
+
+# save in delta lake
+w.write.mode('append').format('delta').saveAsTable("{}.{}".format(DATABASE_NAME, OUTPUT_DATA_TABLE))
--- attic/ACLED-manual-cluster-dv-viz.py
+++ attic/ACLED-manual-cluster-dv-viz.py
@@ -38,12 +38,7 @@ w['n_100'] = w[w >= 100].count(axis=1)
 
 # COMMAND ----------
 
-w[w >= 100].count(axis=1)
-
-# COMMAND ----------
-
 w['prop_5'] = w.apply(lambda x: len(x[x >= 5]) / len(x), axis=1)
-
 
 # COMMAND ----------
 
@@ -51,25 +46,29 @@ w['max_50'] = w.apply(lambda x: 1 if max(x) >= 50 else 0, axis=1)
 
 # COMMAND ----------
 
-w['max_50'].sum()
+w['max_100'] = w.apply(lambda x: 1 if max(x) >= 100 else 0, axis=1)
+
+# COMMAND ----------
+
+w['max_20'] = w.apply(lambda x: 1 if max(x) >= 20 else 0, axis=1)
 
 # COMMAND ----------
 
 def trend_cat(row):
     if ((row['prop_5'] >= .2) and (row['max_100'] == 1)) or (row['n_100'] >=3):
         return 1
-    elif (row['prop_5'] < .2) and (row['max_50'] == 0):
+    elif (row['prop_5'] < .1) and (row['max_50'] == 0): 
         return 3
     else:
         return 2
 
 # COMMAND ----------
 
-w['trend_cat7'] = w.apply(lambda row: trend_cat(row), axis=1)
+w['trend_cat11'] = w.apply(lambda row: trend_cat(row), axis=1)
 
 # COMMAND ----------
 
-w.groupby('trend_cat7').size()
+w.groupby('trend_cat11').size()
 
 # COMMAND ----------
 
@@ -77,7 +76,7 @@ escalation_data = pd.merge(escalation_data, w.iloc[:, -1], on='ADMIN1', how='lef
 
 # COMMAND ----------
 
-cdf = escalation_data[escalation_data['trend_cat7']==1]
+cdf = escalation_data[escalation_data['trend_cat10']==1]
 ad = cdf['ADMIN1'].unique()
 
 for a in ad:
@@ -99,7 +98,7 @@ for a in ad:
 
 # COMMAND ----------
 
-cdf = escalation_data[escalation_data['trend_cat7']==2]
+cdf = escalation_data[escalation_data['trend_cat11']==2]
 ad = cdf['ADMIN1'].unique()
 
 for a in ad:
@@ -121,11 +120,28 @@ for a in ad:
 
 # COMMAND ----------
 
-cdf = escalation_data[escalation_data['trend_cat7']==3]
-ad = cdf['ADMIN1'].unique()
-ad = np.random.choice(ad, 20, replace=False)
+w[(w['trend_cat10']==3) & (w['prop_0'] > .25)]
 
-for a in ad:
+# COMMAND ----------
+
+'Bari', 'Gedaref', 'Kampala', 'Nairobi', 'Abyei', 'Garissa', 'Isiolo', 'Lamu', 'Marsabit', 'Turkana'
+
+# COMMAND ----------
+
+w[(w['trend_cat10']==3) & (w['prop_5'] > .05)]
+
+# COMMAND ----------
+
+cdf
+
+# COMMAND ----------
+
+cdf = escalation_data[escalation_data['ADMIN1'].isin(['Bari', 'Gedaref', 'Kampala', 'Nairobi', 'Abyei', 'Garissa', 'Isiolo', 'Lamu', 'Marsabit', 'Turkana'])]
+ad = cdf['ADMIN1'].unique()
+#ad = np.random.choice(ad, 20, replace=False)
+
+for a in ['Bari', 'Gedaref', 'Kampala', 'Nairobi', 'Abyei', 'Garissa', 'Isiolo', 'Lamu', 'Marsabit', 'Turkana']:
+    print(a)
     plt.figure(figsize=(30, 10)) 
 
     # Plotting the fatalities over time by country
@@ -134,7 +150,7 @@ for a in ad:
         adm1_data = adm1_data[adm1_data['ADMIN1']==a]
         adm1_data = adm1_data.sort_values('STARTDATE')
         plt.plot(adm1_data['STARTDATE'], adm1_data['FATALSUM'], label=adm1)
-        plt.ylim(0, 100)
+        plt.ylim(0, 500)
         
     plt.xlabel('Time')
     plt.xticks(rotation=90)
